04052098b945c1a5037392e1a1c83c595334a958
brieucdlf <2735645+brieucdlf@users.noreply.github.com>
2019-10-21 13:44:09 +0000

    Update lib

--- projects/ngx-tour-md-menu/src/lib/ngx-md-menu-tour.service.ts
+++ projects/ngx-tour-md-menu/src/lib/ngx-md-menu-tour.service.ts
@@ -3,5 +3,7 @@ import { TourService } from 'ngx-tour-core';
 
 import { INgxmStepOption } from './step-option.interface';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class NgxmTourService extends TourService<INgxmStepOption> {}
